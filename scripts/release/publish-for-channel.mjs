62cab9412c36817e05d5fb602b9c5a653bee2bb4
ice breaker <1324318532@qq.com>
2026-08-14 09:42:25 +0800

    fix(ci): keep release publish script idempotent

--- scripts/release/publish-for-channel.mjs
+++ scripts/release/publish-for-channel.mjs
@@ -7,10 +7,6 @@ await execa('pnpm', ['turbo', 'run', 'build', 'lint', 'test', 'test:types'], {
   stdio: 'inherit',
 })
 
-await execa('pnpm', ['changeset', 'version'], {
-  stdio: 'inherit',
-})
-
 await execa('pnpm', ['changeset', 'publish', '--tag', npmTag], {
   stdio: 'inherit',
 })
